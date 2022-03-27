df0790419152baaf10ed742f3bf33654ecb20c9d
tonimobin <85210617+tonimobin@users.noreply.github.com>
2022-03-27 13:16:59 +0300

    Add title, price, loc scraping and printing

--- scrape.py
+++ scrape.py
@@ -1,14 +1,17 @@
 from bs4 import BeautifulSoup
 import requests 
 
-url = "https://www.tori.fi/uusimaa/asunnot/vuokrattavat_asunnot?ca=18&cg=1010&st=u&c=1014&m=313&w=118"
+url = "https://www.tori.fi/uusimaa?q=&cg=1010&w=3&st=u&c=1014&ros=&roe=&ss=&se=&ht=&at=&mre=&ca=18&l=0&md=th"
 page = requests.get(url)
 
 soup = BeautifulSoup(page.content, 'html.parser')
-lists = soup.find_all('section', class_="item_row_flex")
+lists = soup.find_all('a', class_="item_row_flex")
 
 for list in lists:
-    title = list.find('a', class_="")
-    price = list.find('a', class_="")
-    apt_type = list.find('a', class_="")
-    year = list.find('a', class_="")
+    title = list.find('div', class_="li-title").text
+    price = list.find('p', class_="list_price").text
+    location = list.find('div', attrs={"class":["cat_geo", "cat_geo clean_links"]})
+    location = location.find('p').text
+    info = [title, price, location.strip()]
+    
+    print("info: ", info)
