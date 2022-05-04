05ddeeb5c8356d77b86e309f877898351b86e885
tonimobin <toni.mobin+github@gmail.com>
2022-05-04 11:23:35 +0300

    Add empty data handling, remove outdated code

--- app.py
+++ app.py
@@ -192,127 +192,6 @@ app.layout = html.Div(className="my-dash-app", children=[
         #html.Span(""),
     ])
 ])
-# app.layout = dbc.Container([
-#     # Store
-#     dcc.Store(id="memory-output"),
-#     # Title
-#     dbc.Row([
-#         dbc.Col(
-#             html.Div([
-#             html.Span(id="title-p1", children="tori.fi"),
-#             html.Span(id="title-p2", children=" Housing Analysis"),
-#             html.Br(),
-#         ], style={"width": "100%", "display": "flex", "alignItems": "start", "justifyContent": "start"}),
-#         )
-#     ]),
-#     # Main data-graph and query options
-#     dbc.Row([
-#         # data-graph
-#         dbc.Col(
-#             html.Div(children=[
-#                 # dcc.Graph(
-#                 #     id="scatter-chart", className="dbc"
-#                 # )]),
-#                 ]),
-#                 width={"size": 8}
-#         ),
-#         # query options
-#         dbc.Col(
-#             html.Div(children=[
-#                 html.Label("Type:"),
-#                 dcc.Dropdown(
-#                     id="housing-type-dropdown",
-#                     options=[{"label": x, "value": x} for x in housing_types],
-#                     placeholder="Select housing type",
-#                     multi=True,
-#                 ),
-#                 html.Label("Location:"),
-#                 dcc.Dropdown(
-#                     id="location-dropdown",
-#                     options=[{"label": x, "value": x} for x in locations],
-#                     placeholder="Select location",
-#                     multi=True
-#                 ),
-#                 html.Label(id="price-label", children="Price:"),
-#                 dcc.RangeSlider(
-#                     id="price-slider",
-#                     className="dbc",
-#                     min=df.Price.min(),
-#                     max=df.Price.max(),
-#                     value=[df.Price.min(), df.Price.max()],
-#                     step=5000,
-#                     marks=None,
-#                     tooltip={"placement": "bottom", "always_visible": False}
-#                 ),
-#                 html.Label(id="year-label", children="Year:"),
-#                 dcc.RangeSlider(
-#                     id="year-slider",
-#                     className="dbc",
-#                     min=df.Year.min(),
-#                     max=df.Year.max(),
-#                     value=[df.Year.min(), df.Year.max()],
-#                     step=1,
-#                     marks=None,
-#                     tooltip={"placement": "bottom", "always_visible": False}
-#                 ),
-#                 html.Label(id="size-label", children="Size:"),
-#                 dcc.RangeSlider(
-#                     id="size-slider",
-#                     className="dbc",
-#                     min=df.Size.min(),
-#                     max=df.Size.max(),
-#                     value=[df.Size.min(), df.Size.max()],
-#                     step=1,
-#                     marks=None,
-#                     tooltip={"placement": "bottom", "always_visible": False}
-#                 ),
-#                 html.Label("Rooms"),
-#                 dcc.Checklist(
-#                     id="rooms-checklist",
-#                     className="dbc",
-#                     options=[{"label": " " + x, "value": x} for x in rooms],
-#                     inline=False,
-#                     labelStyle={"display": "block"},
-#                     style={"width": 200, "overflow": "auto"},
-#                     labelClassName="mr-1"
-#                 ),
-#             ], className="graph__container"),
-#             width={"size": 3, "offset": 1}
-#         ),
-#     ], justify="around"),
-#     # *** Animation demo ***
-#     html.Div(id="test-div", className="test-div", **{"data-target": 999}, children="Animated number demo: "),
-#     # Key figures
-#     dbc.Row([
-#         dbc.Col(children=[
-#             html.Div(id="key-figures"),
-#         ])
-#     ]),
-
-#     # Pie charts
-#     dbc.Row([
-#         html.H4("Distributions of listings by room and type"),
-#         dbc.Col(
-#             dcc.Graph(id="rooms-pie"), width="4"
-#         ),
-#         dbc.Col(    
-#             dcc.Graph(id="types-pie"), width="4"
-#         )
-#     ], justify="start"),
-#     dbc.Row([
-#         dbc.Col(
-#             dcc.Graph(id="avg-prices-by-size", className="dbc"), width=4
-#             #html.Div(id="pandas-testing")
-#         ),
-#         dbc.Col(
-#             dcc.Graph(id="price-m2-median-by-loc", className="dbc"), width=4
-#         )
-#     ]),
-#     html.H1("******************* TEST ZONE ********************"),
-#     html.Div(id="test-chart", children=[
-#         dcc.Graph(id="scatter-chart")
-#     ])
-# ], fluid=True, className="dbc")
 
 # Handle updates to data when user makes different queries
 @app.callback(
@@ -349,86 +228,22 @@ def update_price_horizontal(data):
     if data is None:
         raise PreventUpdate
     df = pd.DataFrame(data)
-    # dff = df["Location"].value_counts().reset_index()
-    # dff.columns = ["Location", "Counts"]
-    location_grp = df.groupby("Location")
-    dff = location_grp["Price_M2"].median().round(0)
-    dff = dff.reset_index()
-    dff.columns = ["Location", "€/m²"]
-    dff = dff.sort_values("€/m²", ascending=False)
-    figure = px.bar(dff, x="€/m²", y="Location", orientation="h", labels={"Location": "", "€/m²": ""}, text="€/m²")
-    figure.update_layout({"plot_bgcolor": "rgba(0,0,0,0)"}), 
-    # title= {"text": "Median €/m² by location", "x": 0.01, "xanchor": "left", "font_family": "Fira Code", "font_size": 18})
-    figure.update_traces(marker_color="#58B505", textposition="outside", cliponaxis=False, texttemplate='%{text} €')
-    return figure
-
-# Update box plot
-@app.callback(Output("price-distrib", "figure"), Input("memory-output", "data"))
-def update_box_plot(data):
-    if data is None:
-        raise PreventUpdate
-    df = pd.DataFrame(data)
-    # figure = go.Figure(data=[go.Box(y = df["Price"], boxpoints="all")])
-    # figure = px.strip(df["Price"], y="Price")
-    prices = df["Price"]
-    figure = go.Figure()
-    figure.add_trace(go.Box(y = prices, marker_color="#58B505", boxpoints=False, name="Price"))
-    figure.update_layout({"plot_bgcolor": "rgba(0,0,0,0)"})
-    return figure
-# # Update count of listings by location (horizontal bar chart)
-# @app.callback(Output("avg-prices-by-size", "figure"), Input("memory-output", "data"))
-# def update_count_horizontal(data):
-#     if data is None:
-#         raise PreventUpdate
-#     df = pd.DataFrame(data)
-#     dff = df["Location"].value_counts().reset_index()
-#     dff.columns = ["Location", "Counts"]
-#     figure = px.bar(dff, x="Counts", y="Location", orientation="h", labels={"Location": "", "Counts": ""})
-#     figure.update_layout({"plot_bgcolor": "rgba(0,0,0,0)"}), 
-#     title= {"text": "", "x": 0.01, "xanchor": "left", "font_family": "Fira Code", "font_size": 18})
-#     figure.update_traces(marker_color="#679F96")
-#     return figure
-
-# # Update scatter chart
-# @app.callback(Output("scatter-chart", "figure"), Input("memory-output", "data"))
-# def update_scatter(data):
-#     if data is None:
-#         raise PreventUpdate
-#     df = pd.DataFrame(data)
-#     try:
-#         figure = {
-#                 "data": [
-#                     go.Scatter(
-#                         x=df["Size"],
-#                         y=df["Price"],
-#                         mode="markers",
-#                         marker=dict(
-#                             color="#76A1EF",
-#                             size=4,
-#                             opacity=0.8,
-#                             # line=dict(color='Black', width=1)
-#                         ),
-#                     )],
-#                 "layout": go.Layout(
-#                     title= {"text": "Available listings", "x": 0.01, "xanchor": "left", "font_family": "Fira Code", "font_size": 26},
-#                     xaxis={"title": "Size m²", "showgrid" : False},
-#                     yaxis={"title": "Price €", "showgrid" : False},
-#                 )
-#             }
-#     # If no listings are found with given features, return a message informing the user.
-#     except(KeyError):
-#         figure = {
-#             "layout": {"xaxis": { "visible": "false"},
-#                 "yaxis": { "visible": "false" },
-#                 "annotations": [
-#                     {"text": "No available listings with given features.", "xref": "paper", "yref": "paper",
-#                         "showarrow": "false", "font": { "size": 28 }
-#                     }
-#                 ]
-#             }
-#         }
-#         return figure
-#     return figure
+    if len(df) > 0:
+        location_grp = df.groupby("Location")
+        dff = location_grp["Price_M2"].median().round(0)
+        dff = dff.reset_index()
+        dff.columns = ["Location", "€/m²"]
+        dff = dff.sort_values("€/m²", ascending=False)
+        figure = px.bar(dff, x="€/m²", y="Location", orientation="h", labels={"Location": "", "€/m²": ""}, text="€/m²")
+        figure.update_layout({"plot_bgcolor": "rgba(0,0,0,0)"}), 
+        figure.update_traces(marker_color="#58B505", textposition="outside", cliponaxis=False, texttemplate='%{text} €')
+        return figure
+    else:
+        figure = go.Figure(data=[])
+        figure.update_layout({"plot_bgcolor": "rgba(0,0,0,0)"})
+        figure.update_xaxes(visible=False)
+        figure.update_yaxes(visible=False)
+        return figure
 
 # Create & update keyfigures
 @app.callback(Output("key-figures", "children"), Input("memory-output", "data"))
@@ -458,7 +273,7 @@ def update_keyfig(data):
     else:
         return [
             html.Div(
-                html.P("No listings with given options.")
+                html.P(className="empty-data-error", children="No listings with given options.")
             )
         ]
 
@@ -488,57 +303,68 @@ def update_map(data):
     if data is None:
         raise PreventUpdate
     df = pd.DataFrame(data)
-    # create a new df with location and count of listings in the location
-    loc_counts = df["Location"].value_counts().reset_index()
-    loc_counts.columns = ["Location", "Count"]
-    locations = ["Helsinki", "Lappi"]
-    figure = px.choropleth(loc_counts, geojson=finland, locations="Location", color="Count", color_continuous_scale="Greens", range_color=(0,12), scope="europe")
-    figure.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-    return figure
-# # Update key figures
-# @app.callback(Output("key-figures", "children"),
-#               Input("memory-output", "data"))
-# def update_keyfigs(data):
-#     if data is None:
-#         raise PreventUpdate
-#     df = pd.DataFrame(data)
-#     if len(df) > 0:
-#         return [
-#             html.H4(id="key-figs-title", children="Key figures"),
-#             html.Div(id="key-figs", children=[
-#                 dbc.Row([
-#                     dbc.Row([
-#                         dbc.Col(html.P(id="key-fig", children="Amount of listings", className="text-center"), width=2),
-#                         dbc.Col(html.P(id="key-fig", children="Mean €/m²", className="text-center"), width=2),
-#                         dbc.Col(html.P(id="key-fig", children="Median year of construction", className="text-center"), width=2)
-#                     ], justify="start"),
-#                     dbc.Row([
-#                         dbc.Col(html.P(f"{len(df)}", className="text-center"), width=2),
-#                         dbc.Col(html.P(children="{:,} €".format(math.floor(df.Price.sum() / df.Size.sum())).replace(",", " "), className="text-center"), width=2),
-#                         dbc.Col(html.P(f"{math.floor(df.Year.median())}", className="text-center"), width=2)
-#                     ], justify="start"),
-#                 ], justify="start"),
-#                 dbc.Row([
-#                     dbc.Row([
-#                         dbc.Col(html.P(id="key-fig", children="Cheapeast listing", className="text-center"), width=2),
-#                         dbc.Col(html.P(id="key-fig", children="Most expensive", className="text-center"), width=2),
-#                         dbc.Col(html.P(id="key-fig", children="Mean price", className="text-center"), width=2)
-#                     ]),
-#                     dbc.Row([
-#                         dbc.Col(html.P(children="{:,} €".format(df.Price.min()).replace(",", " "), className="text-center"), width=2),
-#                         dbc.Col(html.P(children="{:,} €".format(df.Price.max()).replace(",", " "), className="text-center"), width=2),
-#                         dbc.Col(html.P(children="{:,} €".format(math.floor(df.Price.mean())).replace(",", " "), className="text-center"), width=2)
-#                     ]),
-#                 ], justify="start")
-#             ]),
-#         ]
-#     else:
-#         return [
-#             html.Div(
-#                 html.P("-")
-#             )
-#         ]
+    if len(df) > 0:
+        # create a new df with location and count of listings in the location
+        loc_counts = df["Location"].value_counts().reset_index()
+        loc_counts.columns = ["Location", "Count"]
+        locations = ["Helsinki", "Lappi"]
+        figure = px.choropleth(loc_counts, geojson=finland, locations="Location", color="Count", color_continuous_scale="Greens", range_color=(0,12), scope="europe")
+        figure.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+        return figure
+    else:
+        figure = go.Figure(data=[])
+        figure.update_layout({"plot_bgcolor": "rgba(0,0,0,0)"})
+        figure.update_xaxes(visible=False)
+        figure.update_yaxes(visible=False)
+        return figure
 
+# Update price distribution box plot
+@app.callback(Output("price-distrib", "figure"), Input("memory-output", "data"))
+def update_box_plot(data):
+    if data is None:
+        raise PreventUpdate
+    df = pd.DataFrame(data)
+    if len(df) > 0:
+        prices = df["Price"]
+        figure = go.Figure()
+        figure.add_trace(go.Box(y = prices, marker_color="#58B505", boxpoints=False, name="Price"))
+        figure.update_layout({"plot_bgcolor": "rgba(0,0,0,0)"})
+        return figure
+    else:
+        figure = go.Figure(data=[])
+        figure.update_layout({"plot_bgcolor": "rgba(0,0,0,0)"})
+        figure.update_xaxes(visible=False)
+        figure.update_yaxes(visible=False)
+        return figure
+
+# # Update types pie chart
+@app.callback(Output("types-pie", "figure"),
+              Input("memory-output", "data"))
+def update_types_pie(data):
+    colors = ["#0B2027", "#40798C", "#70A9A1", "#CFD7C7", "#F6F1D1", "E9E0A6"]
+    if data is None:
+         raise PreventUpdate
+    df = pd.DataFrame(data)
+    if len(df) > 0:
+        types = df["Type"].unique()
+        type_count = df["Type"].value_counts(sort=False).array
+        figure = go.Figure(data=[go.Pie(labels=types, values=type_count)], 
+        layout=go.Layout(margin={"t" : 0, "b" : 0}, height=250))
+        figure.update_traces(
+            hoverinfo="value", 
+            textinfo="label+percent", 
+            marker=dict(colors = colors), 
+            hole=0.5, 
+            textposition="outside",
+        )
+        figure.update(layout_showlegend=False)
+        return figure
+    else:
+        figure = go.Figure(data=[])
+        figure.update_layout({"plot_bgcolor": "rgba(0,0,0,0)"})
+        figure.update_xaxes(visible=False)
+        figure.update_yaxes(visible=False)
+        return figure
 
 #Update rooms pie chart (to:do prevent empty dataset error for this & type)
 @app.callback(Output("rooms-pie", "figure"),
@@ -549,42 +375,27 @@ def update_rooms_pie(data):
     if data is None:
          raise PreventUpdate
     df = pd.DataFrame(data)
-    rooms = df["Rooms"].unique()
-    room_count = df["Rooms"].value_counts(sort=False).array
-    figure = go.Figure(data=[go.Pie(labels=rooms, values=room_count)], layout=go.Layout(margin={"t" : 0, "b" : 0}, height=250))
-    figure.update_traces(
-        hoverinfo="value", 
-        textinfo="label+percent",
-        marker=dict(colors = colors), 
-        hole=0.5, 
-        textposition="outside",
-        
-    )
-    figure.update(layout_showlegend=False)
-    return figure
+    if len(df) > 0:
+        rooms = df["Rooms"].unique()
+        room_count = df["Rooms"].value_counts(sort=False).array
+        figure = go.Figure(data=[go.Pie(labels=rooms, values=room_count)], layout=go.Layout(margin={"t" : 0, "b" : 0}, height=250))
+        figure.update_traces(
+            hoverinfo="value", 
+            textinfo="label+percent",
+            marker=dict(colors = colors), 
+            hole=0.5, 
+            textposition="outside",
+            
+        )
+        figure.update(layout_showlegend=False)
+        return figure
+    else:
+        figure = go.Figure(data=[])
+        figure.update_layout({"plot_bgcolor": "rgba(0,0,0,0)"})
+        figure.update_xaxes(visible=False)
+        figure.update_yaxes(visible=False)
+        return figure
     
-
-# # Update types pie chart
-@app.callback(Output("types-pie", "figure"),
-              Input("memory-output", "data"))
-def update_types_pie(data):
-    colors = ["#0B2027", "#40798C", "#70A9A1", "#CFD7C7", "#F6F1D1", "E9E0A6"]
-    if data is None:
-         raise PreventUpdate
-    df = pd.DataFrame(data)
-    types = df["Type"].unique()
-    type_count = df["Type"].value_counts(sort=False).array
-    figure = go.Figure(data=[go.Pie(labels=types, values=type_count)], 
-    layout=go.Layout(margin={"t" : 0, "b" : 0}, height=250))
-    figure.update_traces(
-        hoverinfo="value", 
-        textinfo="label+percent", 
-        marker=dict(colors = colors), 
-        hole=0.5, 
-        textposition="outside",
-    )
-    figure.update(layout_showlegend=False)
-    return figure
 if __name__ == "__main__":
     app.run_server(debug=True)
 
